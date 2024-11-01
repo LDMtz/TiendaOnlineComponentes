bfbc22626159a77b0151b892f8d77704c106751f
LDMtz <leonciodanielmartinez@gmail.com>
2024-10-31 23:35:17 -0700

    Header y Footer arreglados

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,33 +1,39 @@
 <!-- Pie de la página -->
 
-<footer class="fixed bottom-0 w-full pb-4">
-    <hr class="bg-cm-gray-5 mx-5 h-1">
-  <div class="flex justify-left text-cm-gray-5 pl-4 pb-1">
-    <div class="flex">
-      <div class="">© 2024 Belikekis Components. Todos los derechos reservados.</div>
-      <div class="ml-20">Términos</div>
-      <div class="ml-6">Privacidad</div> 
-      <div class="ml-6">Contacto</div>
-</div>
-    <div class="text-2xl flex ml-auto mr-20 items-center">
-        <a href="https://wa.me/" target="_blank" class="ml-2">
-        <i class="fa-brands fa-whatsapp"></i>
-        </a>
+<footer class="fixed bottom-0 w-full">
+    <hr class="bg-cm-gray-5 mx-5 h-0.5 mb-3">
 
-        <a href="https://www.facebook.com/" target="_blank" class="ml-2">
-        <i class="fa-brands fa-facebook"></i>
-        </a>
+    <!-- <div class="w-full"> -->
+      <div class="flex items-center text-cm-gray-5 px-8 mb-3">
+          <p class="">© 2024 Belikekis Components. Todos los derechos reservados.</p>
+          <p class="ml-20">Términos</p>
+          <p class="ml-6">Privacidad</p> 
+          <p class="ml-6">Contacto</p>
 
-        <a href="https://www.instagram.com/" target="_blank" class="ml-2">
-        <i class="fa-brands fa-instagram"></i>
-        </a>
+          <div class="ml-auto text-2xl space-x-5">
+            <a href="https://wa.me/" target="_blank">
+              <i class="fa-brands fa-whatsapp"></i>
+            </a>
 
-        <a href="https://github.com/" target="_blank" class="ml-2">
-        <i class="fa-brands fa-github"></i>
-        </a>
+            <a href="https://www.facebook.com/" target="_blank">
+              <i class="fa-brands fa-facebook"></i>
+            </a>
 
-        <a href="https://x.com/" target="_blank" class="ml-2">
-        <i class="fa-brands fa-square-x-twitter"></i>
-        </a>
-    </div>   
+            <a href="https://www.instagram.com/" target="_blank">
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+
+            <a href="https://github.com/" target="_blank">
+              <i class="fa-brands fa-github"></i>
+            </a>
+
+            <a href="https://x.com/" target="_blank">
+              <i class="fa-brands fa-square-x-twitter"></i>
+            </a>
+          </div>
+      </div>
+    <!-- </div> -->
+
+
+      
 </footer>
