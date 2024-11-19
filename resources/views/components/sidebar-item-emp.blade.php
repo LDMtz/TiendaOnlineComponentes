bf5506281b4934abf9a04282e27f598b2639daae
LDMtz <leonciodanielmartinez@gmail.com>
2024-11-18 23:14:20 -0700

    Modificacion item-sidebar empleado

--- resources/views/components/sidebar-item-emp.blade.php
+++ resources/views/components/sidebar-item-emp.blade.php
@@ -1,8 +1,8 @@
 @props(['icon','route'])
 
 <li class="mx-3 p-2 flex items-center bg-cm-gray-3 rounded-lg hover:border-x-8 border-cm-blue-6">
-    <i class="{{ $icon ?? 'fa-solid fa-user' }} text-cm-blue-9 mr-4 ml-8"></i>
     <a href="{{ route($route)}}" class="text-cm-blue-9 font-bold">
-        {{ $slot }}
+        <i class="{{ $icon ?? 'fa-solid fa-user' }} text-cm-blue-9 mr-4 ml-8"></i>
+        <span>{{ $slot }}</span>
     </a>
 </li>
