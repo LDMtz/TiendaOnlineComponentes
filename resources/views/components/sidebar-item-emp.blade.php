6689712b0aa9cb48d52f02965cb5534a07b4c54e
LDMtz <leonciodanielmartinez@gmail.com>
2024-11-19 17:37:24 -0700

    Creacion index de la seccion proveedores

--- resources/views/components/sidebar-item-emp.blade.php
+++ resources/views/components/sidebar-item-emp.blade.php
@@ -1,7 +1,7 @@
 @props(['icon','route'])
 
-<li class="mx-3 p-2 flex items-center bg-cm-gray-3 rounded-lg hover:border-x-8 border-cm-blue-6">
-    <a href="{{ route($route)}}" class="text-cm-blue-9 font-bold">
+<li class="mx-3 flex items-center bg-cm-gray-3 rounded-lg hover:border-x-8 border-cm-blue-6">
+    <a href="{{ route($route)}}" class="text-cm-blue-9 font-bold p-2">
         <i class="{{ $icon ?? 'fa-solid fa-user' }} text-cm-blue-9 mr-4 ml-8"></i>
         <span>{{ $slot }}</span>
     </a>
--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -5,7 +5,34 @@
             <x-sidebar-employee/>
         </div>
         <div class="flex-1 px-4 ml-8 my-8">
-            @php
+
+            <!-- Sección  -->
+            <div class="flex items-center space-x-5">
+                <a href="{{route('home')}}" class="w-7 h-7 flex items-center justify-center rounded-full bg-cm-blue-10 p-1 hover:bg-blue-400">
+                    <i class="fa-solid fa-arrow-left text-white text-lg"></i>
+                </a>
+                <h1 class="text-cm-blue-10 font-black text-3xl" >SECCIÓN PROVEEDORES</h1>
+            </div>
+
+            <!-- Banner tabla -->
+            <div class="flex items-center bg-cm-gray-2 rounded-lg border-2 border-cm-gray-3 mt-7 p-3">
+                <p class="text-white font-bold text-lg">Lista de Proveedores</p>
+                <!-- Buttons -->
+                <div class="flex space-x-4 ml-auto">
+                    <a href="#" class="bg-cm-blue-9 text-white py-1 px-3 rounded-lg hover:bg-sky-500 hover:text-sky-100">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <span class="font-semibold ml-1">BUSCAR</span>
+                    </a>
+                    <a href="#" class="bg-cm-green-3 text-white py-1 px-3 rounded-lg hover:bg-green-500 hover:text-green-100">
+                        <i class="fa-solid fa-plus"></i>
+                        <span class="font-semibold ml-1">AGREGAR</span>
+                    </a>
+                </div>
+            </div>
+
+            <!-- Tabla -->
+            <div class="bg-cm-gray-2 rounded-lg border-2 border-cm-gray-3 mt-6 p-4">
+                @php
                 $users = [
                     [
                         'id' => 1,
@@ -48,36 +75,38 @@
                         'created_at' => '2020-08-20 16:00:00',  // Fecha de creación
                     ]
                 ];
+                @endphp
+                <x-table>
+                    <x-slot name="headTable">
+                        <x-th-table>Id</x-th-table>
+                        <x-th-table>Nombre</x-th-table>
+                        <x-th-table>Apellido</x-th-table>
+                        <x-th-table>Estado</x-th-table>
+                        <x-th-table>Email</x-th-table>
+                        <x-th-table>Edad</x-th-table>
+                        <x-th-table>Rol</x-th-table>
+                        <x-th-table>Fecha</x-th-table>
+                        <x-th-table>Acciones</x-th-table>
+                    </x-slot>
+                    <x-slot name="bodyTable">
+                        @foreach($users as $user)
+                        <tr>
+                            <x-td-table type="normal" :content="$user['id']" />
+                            <x-td-table type="normal" :content="$user['name']" />
+                            <x-td-table type="normal" :content="$user['lastname']" />
+                            <x-td-table type="state" :content="$user['state']" />
+                            <x-td-table type="normal" :content="$user['email']" />
+                            <x-td-table type="normal" :content="$user['age']" />
+                            <x-td-table type="normal" :content="$user['role']" />
+                            <x-td-table type="normal" :content="$user['created_at']" />
+                            <x-td-table type="actions" :content="['section' => 'supplier', 'id' => $user['id']]" />
+                        </tr>
+                        @endforeach
+                    </x-slot>
+                </x-table>
+            </div>
 
-            @endphp
-            <x-table>
-                <x-slot name="headTable">
-                    <x-th-table>Id</x-th-table>
-                    <x-th-table>Nombre</x-th-table>
-                    <x-th-table>Apellido</x-th-table>
-                    <x-th-table>Estado</x-th-table>
-                    <x-th-table>Email</x-th-table>
-                    <x-th-table>Edad</x-th-table>
-                    <x-th-table>Rol</x-th-table>
-                    <x-th-table>Fecha</x-th-table>
-                    <x-th-table>Acciones</x-th-table>
-                </x-slot>
-                <x-slot name="bodyTable">
-                    @foreach($users as $user)
-                    <tr>
-                        <x-td-table type="normal" :content="$user['id']" />
-                        <x-td-table type="normal" :content="$user['name']" />
-                        <x-td-table type="normal" :content="$user['lastname']" />
-                        <x-td-table type="state" :content="$user['state']" />
-                        <x-td-table type="normal" :content="$user['email']" />
-                        <x-td-table type="normal" :content="$user['age']" />
-                        <x-td-table type="normal" :content="$user['role']" />
-                        <x-td-table type="normal" :content="$user['created_at']" />
-                        <x-td-table type="actions" :content="['section' => 'supplier', 'id' => $user['id']]" />
-                    </tr>
-                    @endforeach
-                </x-slot>
-            </x-table>
+
 
         </div>
     </div>
